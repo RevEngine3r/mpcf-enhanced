0fd5598593865aa9ced6108aa6f99a2e4f852a44
RevEngine3r <revengine3r@gmail.com>
2026-02-21 23:04:33 +0330

    Add new configuration URL to user settings

--- src/user_settings.py
+++ src/user_settings.py
@@ -15,6 +15,7 @@ SOURCE_URLS = [
     "https://raw.githubusercontent.com/Freedom-Guard-Builder/FL/refs/heads/main/config/Fast.txt",
     "https://raw.githubusercontent.com/Ashkan-m/v2ray/main/Sub.txt",
     "https://raw.githubusercontent.com/davudsedft/purvpn/refs/heads/main/links/purkow.txt",
+    "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt",
 ]
 
 # Set to True to fetch the maximum possible number of configurations.
